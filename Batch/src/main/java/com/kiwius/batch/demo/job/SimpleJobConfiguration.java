401085d3857ed9c869d268a84625b0ad181ac914
suyongHan <3519zx@naver.com>
2021-02-22 20:10:57 +0900

    접속정보변경 및 batch parameter 예제 실습

--- Batch/src/main/java/com/kiwius/batch/demo/job/SimpleJobConfiguration.java
+++ Batch/src/main/java/com/kiwius/batch/demo/job/SimpleJobConfiguration.java
@@ -1,10 +1,12 @@
 package com.kiwius.batch.demo.job;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -19,16 +21,18 @@ public class SimpleJobConfiguration {
 
     @Bean
     public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob").start(simpleStep1()).build();
+        return jobBuilderFactory.get("simpleJob").start(simpleStep1(null)).build();
         //'simple' job이란 이름의 batch job을  생성
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1").tasklet((contribution, chunkContext) -> {
             // 'simpleStep1'이란 이름의 batch step을  생성
             // step에서 수행될 tasklet을 생성
             log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>this is step1");
+            log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>requestDate {}", requestDate);
             return RepeatStatus.FINISHED;
         }).build();
     }
